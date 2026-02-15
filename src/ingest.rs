ad865636344d441986219aab4689361d24c78014
Karthik Ramesh Kamath <31042314+karthikramesh55@users.noreply.github.com>
2026-02-15 00:08:07 +0000

    [commit] Ingestion + Data Processing
    Phase 2, Step 2

--- src/ingest.rs
+++ src/ingest.rs
@@ -1,4 +1,7 @@
-pub fn ingest_url(url: &str) -> Result<(), Box<dyn std::error::Error>>
+use reqwest::Url;
+use readability::extractor;
+
+pub async fn ingest_url(url: &str) -> Result<(), Box<dyn std::error::Error>>
 {
     if !url.starts_with("http") || !url.starts_with("https")
     {
@@ -6,10 +9,35 @@ pub fn ingest_url(url: &str) -> Result<(), Box<dyn std::error::Error>>
         Note: The "string".into() converts the string into the return type we need (Box<dyn std::error::Error>).
               This makes the valid error propagation work seamlessly.
         */
-        return Err(format!("Invalid target URL: '{}'. Must start with http or https", url).into());
+        return Err(format!("Invalid target URL: '{}'. Must start with http or https accordingly", url).into());
     }
 
-    println!("Ingesting the target URL: {}", url);
-    Ok(())
+    println!("Accessing the URL: {}", url);
 
+    /*
+    Note: We are fetching the raw HTML using the reqwest crate utility in an asynchronous manner
+          The .await enables the yielding of control of the task, until the network transaction finishes
+          Meanwhile, the control moves to another task and comes back to this task after completion of the network transaction
+    */
+    let response_text = reqwest::get(url).await?.text().await?;
+
+    println!("Fetched {} bytes from the URL. Extracting the text content...", response_text.len());
+
+    /*
+    Note: We are parsing the raw HTML using the readability::extractor crate utility to get the primary text content
+             In this regard, the extractor requires a stream wrapper (i.e. cursor objectual body) + URL objectual body to resolve relative links
+          The cursor that is wrapping the response buffer implements Read + Seek, that is used to read + navigate within the in-memory data.
+             We make the cursor objectual body mutable so its internal position can be updated during read + seek operations.
+    */
+    let mut cursor = std::io::Cursor::new(response_text);
+    let url_objectual_body = Url::parse(url)?;
+
+    let resultant_product = extractor::extract(&mut cursor, &url_objectual_body)?;
+
+    println!("---------------------------------------------");
+    println!("Title:       {}", resultant_product.title);
+    println!("Content:     {} bytes", resultant_product.text.len());
+    println!("Snippet:     {:.300}...", resultant_product.text);
+
+    Ok(())  // Note: The Ok() describes the successful completion of this flow, and when () is passed as an argument onto Ok(), that describes the unit type for returning of nullity
 }
--- src/main.rs
+++ src/main.rs
@@ -10,11 +10,23 @@ use actions::Action;
 /*
 Note: Result<T, E> facilitates scenario handling (success, failure), where:
 - T describes any successful handling
-  For example: () is a unit type for describing the signaling of "the flow has ended okay" while returning nullity
+  For example: () is a unit type for describing the aspect of "the flow has ended okay" while returning nullity
 - E describes any error handling
   For example: Box<dyn std::error::Error> describes the accommodation of any encountered error
+
+Note: The blocking aspect dedicates system time and effort on a single task
+      The "async" keyword allows a function to yield control of a task while waiting for I/O
+      This delivers an efficient use of system time and effort
+
+Note: The tokio::main macro transforms the asynchronous main function into a synchronous one 
+         for initalizing the Tokio runtime scheduler and executes the asynchronous source code
+      Since we have transformed the main function into an asynchronous function,
+         any function call inside that whichever performs I/O should be transformed into an asychronous function as well. 
 */
-fn main() -> Result<(), Box<dyn std::error::Error>>
+
+#[tokio::main]
+
+async fn main() -> Result<(), Box<dyn std::error::Error>>
 {
     let args: Vec<String> = env::args().collect();
 
@@ -58,13 +70,15 @@ fn main() -> Result<(), Box<dyn std::error::Error>>
             let value = do_something()?;
 
             The '?' operator checks as to whether do_something() returns Err
-            If it does so, the "containing" flow stops, and returns that error automatically
-            In this case: the main flow would stop, and return that error automatically.
+            If it does so, the "containing" flow stops, and returns that error immediately
+            In this case: the main flow would stop, and returns that error immediately.
 
             */
             let target = &args[2];
             let bookmark = models::Bookmark::new(target);
-            ingest::ingest_url(&bookmark.url)?;
+
+            // Note: Since ingest_url has been transformed in to an asynchronous function, .await is used during the call
+            ingest::ingest_url(&bookmark.url).await?;
             println!("A bookmark has been built: {:?}", bookmark); // Note: {:?} facilitates the displaying of any structure
         }
 
@@ -77,5 +91,5 @@ fn main() -> Result<(), Box<dyn std::error::Error>>
         }
     }
 
-    Ok(()) // Note: The Ok() describes the successful completion of the main flow, and when () is passed as an argument onto (), that describes the unit type for returning of nullity
+    Ok(()) // Note: The Ok() describes the successful completion of the main flow, and when () is passed as an argument onto Ok(), that describes the unit type for returning of nullity
 }
