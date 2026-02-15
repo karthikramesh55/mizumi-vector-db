a5fd8675c1ab6391f3e1a335177e5366580a0ac4
Karthik Ramesh Kamath <31042314+karthikramesh55@users.noreply.github.com>
2026-02-15 00:08:07 +0000

    [commit] inflow of command line arguments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+
+fn main()
+{
+    let args: Vec<String> = env::args().collect();
+    let action: String;
+
+    if args.len() < 2
+    {
+        println!("Usage: cargo run <action>");
+        return;
+    }
+
+    action = args[1].clone();
+
+    println!("The action you had requested is: {}", action);
 }
